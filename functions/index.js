d1f812692fc27091dcf9cc04a6a28308f2e05117
bvlion <bvltiggeari@ambitious-i.net>
2021-03-05 00:33:09 +0900

    lint を変更

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,21 @@
 const functions = require("firebase-functions")
-const express = require('express')
-const path = require('path')
+const express = require("express")
+const path = require("path")
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
-app.use(express.static(path.join(__dirname, 'public')))
+app.set("views", path.join(__dirname, "views"))
+app.engine("html", require("ejs").renderFile)
+app.set("view engine", "html")
+app.use(express.static(path.join(__dirname, "public")))
 
-const render = (res, data) => res.render('index.html', data)
+const render = (res, data) => res.render("index.html", data)
 
-app.get('/terms_of_use', (_, res) => render(res, {
+app.get("/terms_of_use", (_, res) => render(res, {
   title: "【シェブ！】利用規約",
   url: functions.config().url.terms
 }))
 
-app.get('/privacy_policy', (_, res) => render(res, {
+app.get("/privacy_policy", (_, res) => render(res, {
   title: "【シェブ！】プライバシーポリシー",
   url: functions.config().url.privacy
 }))
